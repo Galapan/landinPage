3dbbb2a81a9b1209f6f8fdd1a8a2d2d742aa910e
Galapan <bastian4le@gmail.com>
2026-02-06 13:53:20 -0600

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-
+import { Header } from "./components/header"
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <main className="border border-white w-[375px] m-auto">
+      <section className="bg-navy-850 p-6 h-[420px]">
+        <Header />
+      </section>
+    </main>
   )
 }
 
